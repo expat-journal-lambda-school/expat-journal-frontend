50bb5abe548101d436225dfa26617690bfba4738
Tyler Turnipseed <tyler@techturnip.us>
2019-07-24 20:41:15 -0400

    performed some initial scaffolding/folder structure tasks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+// Styles
 import './index.scss'
-import App from './App'
+
+// Components
+import App from './components/App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
