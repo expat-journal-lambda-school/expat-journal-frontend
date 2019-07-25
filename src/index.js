bb03584e4abcdff2500c7a5f910b1d83ee3df7be
Tyler Turnipseed <tyler@techturnip.us>
2019-07-24 20:56:10 -0400

    initialized redux, created reducer, applied middleware and wrapped root component with Provider and Router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import * as serviceWorker from './serviceWorker'
 
 // Styles
 import './index.scss'
@@ -7,6 +8,36 @@ import './index.scss'
 // Components
 import App from './components/App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// Router
+import { BrowserRouter as Router } from 'react-router-dom'
 
-ServiceWorker.unregister()
+// Redux
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+
+// Redux Middleware
+import thunk from 'redux-thunk'
+
+// Reducer
+import rootReducer from './store/reducers'
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : f => f
+  )
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+)
+
+serviceWorker.unregister()
---
+++ src/store/reducers/postsReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  posts: [],
+  isLoading: false,
+  errorMessage: null,
+  isLoggedIn: false
+}
+
+export const postsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
