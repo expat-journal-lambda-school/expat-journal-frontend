141b8f7b56818a07237da8c6a5942054e8f4d243
Tyler Turnipseed <tyler@techturnip.us>
2019-07-27 18:27:54 -0400

    Added conditional rendering in Nav to display Dashboard/Logout if logged in or Login/Sign Up if not logged in

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { logout } from '../../store/actions'
 import { withRouter, Link } from 'react-router-dom'
 
 function Navigation(props) {
+  console.log(props)
+
   // Handle active class
-  const onClick = e => {}
+  const onClick = e => {
+    e.preventDefault()
+
+    const target = e.target.innerHTML
+    console.log(target)
+
+    if (target === 'Logout') {
+      props.logout()
+    }
+  }
 
   return (
     <div className="navbar-fixed">
@@ -23,12 +36,28 @@ function Navigation(props) {
             <li>
               <Link to="/posts">Posts</Link>
             </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/register">Sign Up</Link>
-            </li>
+
+            {props.isLoggedIn ? (
+              <>
+                <li>
+                  <Link to="/login">Dashboard</Link>
+                </li>
+                <li>
+                  <a href="#" onClick={onClick}>
+                    Logout
+                  </a>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+                <li>
+                  <Link to="/register">Sign Up</Link>
+                </li>
+              </>
+            )}
           </ul>
         </div>
       </nav>
@@ -51,4 +80,13 @@ function Navigation(props) {
   )
 }
 
-export default withRouter(Navigation)
+const mapStateToProps = state => ({
+  isLoggedIn: state.authReducer.isLoggedIn
+})
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    { logout }
+  )(Navigation)
+)
